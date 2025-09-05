6dd3da3f57503ee647c8dc716b58ce4bf26b1b63
sent2piya-debug <sent2piya@gmail.com>
2025-09-05 20:09:20 +0700

    Update listing.ts

--- src/types/listing.ts
+++ src/types/listing.ts
@@ -1,12 +1,15 @@
 export type ListingDTO = {
   id: string;
   title: string;
-  province?: string | null;
-  district?: string | null;
+  type?: "LAND" | "HOUSE" | "SERVICE";
   widthM?: number | null;
   lengthM?: number | null;
   landSizeSqm?: number | null;
   priceMin?: number | null;
   priceMax?: number | null;
+  province?: string | null;
+  district?: string | null;
   photos?: string[] | null;
+  description?: string | null;
+  createdAt?: string;
 };
